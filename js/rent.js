c3a31405c9ced5c20d4d5e5ef47cf4059ccf3040
ahuacate15 <carlos.itca@gmail.com>
2020-12-16 20:05:07 -0600

    javascript para renta/venta de peliculas

---
+++ js/rent.js
@@ -0,0 +1,22 @@
+function rentMoviesAjax(detail) {
+    var jwt = localStorage.getItem('jwt');
+
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url : URL_API + '/movie/rented',
+            type : 'POST',
+            data : {
+                detail : detail
+            },
+            headers: {
+                'Authorization':'Bearer ' + jwt
+            },
+            success: function(data) {
+                resolve(data);
+            },
+            error: function(xhr) {
+                reject(xhr);
+            }
+        });
+    });
+}
